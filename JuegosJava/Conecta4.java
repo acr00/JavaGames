e064ced69fb99675b924947b8956e4d600e68ebb
Acr00 <adriancainas@gmail.com>
2024-10-03 10:02:42 +0200

    Actualizado03/10/24

--- JuegosJava/Conecta4.java
+++ JuegosJava/Conecta4.java
@@ -2,140 +2,168 @@ package JuegosJava;
 import java.util.Scanner;
 
 public class Conecta4 {
+   
+    private static final int F = 6; 
+    private static final int C = 7;
+    private static char[][] tablero = new char[F][C]; 
+    private static char jugadorActual = 'X'; 
 
-    private static char jugador1 = 'X';
-    private static char jugador2 = 'O';
-    private static char[] jugadores = {jugador1, jugador2};
-    private static int jugadoractual = 0;
-    private static int filas = 6;
-    private static int columnas = 7;
-    private static char caracter = '*';
-    private static char[][] tablero = new char [filas][columnas];
-    private static Scanner sc = new Scanner(System.in);
-    private static boolean juegoTerminado = false;
+    public static void main(String[] args) {
 
-    public static void partida() { 
-        for (int f = 0; f < filas; f++) {
-            for (int c = 0; c < columnas; c++) {
-                tablero[f][c] = caracter;
-                System.out.print(tablero[f][c] + " ");
+        inicializarTablero(); 
+        boolean juegoTerminado = false; 
+        Scanner scanner = new Scanner(System.in);
+
+        
+        while (!juegoTerminado) {
+
+            mostrarTablero(); 
+            System.out.println("Turno del jugador " + jugadorActual);
+            System.out.println("Elige una columna (1-7): ");
+            int columna = scanner.nextInt() - 1; 
+
+            
+            if (columna >= 0 && columna < C) {
+
+                if (colocarFicha(columna)) { 
+                    if (hayGanador()) { 
+                    mostrarTablero();
+                        System.out.println("¡El jugador " + jugadorActual + " ha ganado!");
+                        juegoTerminado = true; 
+                    } else if (tableroLleno()) { 
+                        mostrarTablero();
+                        System.out.println("¡Empate! El tablero está lleno.");
+                        juegoTerminado = true; 
+                    } else {
+                        cambiarTurno(); 
+                    }
+                } else {
+                    System.out.println("Columna llena. Elige otra columna.");
+                }
+            } else {
+                System.out.println("Columna no válida. Intentalo otra vez.");
             }
-            System.out.println();
         }
-        System.out.println("0 1 2 3 4 5 6");
+
+        System.out.println("¿Quieres jugar otra vez? (s/n): ");
+        char respuesta = scanner.next().charAt(0); 
+        if (respuesta == 's') {
+
+            reiniciarJuego(); 
+
+        }
+        scanner.close();
     }
-    public static void estadoActual() { 
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
+
+    // Inicializa el tablero llenando todas las posiciones con el caracter '-'
+    
+    private static void inicializarTablero() {
+        for (int i = 0; i < F; i++) {
+            for (int j = 0; j < C; j++) {
+                tablero[i][j] = '-';
+            }
+        }
+    }
+
+    // Muestra el tablero de juego en la consola
+
+    private static void mostrarTablero() {
+        for (int i = 0; i < F; i++) {
+            for (int j = 0; j < C; j++) {
                 System.out.print(tablero[i][j] + " ");
             }
-            System.out.println();
+            System.out.println(); 
         }
-        System.out.println("0 1 2 3 4 5 6"); 
+        System.out.println("1 2 3 4 5 6 7"); 
     }
 
-    public static int elegirColumna(char jugador) {
-        System.out.println(" ");
-        System.out.println("Jugador " + "(" + jugadores[jugadoractual] + ")" + " elija una columna (0-6): ");
-        while (!sc.hasNextInt()) {
-            sc.next();
-            System.out.println(" ");
-            System.out.println("Por favor, jugador " + "(" + jugadores[jugadoractual] + ")" + " introduzca un número válido entre 0 y 6:");
-        }
+    // Coloca la ficha del jugador actual en la columna elegida, si es posible
 
-        return sc.nextInt();
-    }
-
-    public static boolean verificarColumna(int columna){
-        return columna >= 0 && columna < columnas && tablero[0][columna] == caracter;
-    }
-
-    public static int ponerFicha(int columna, char ficha) {
-        for(int i = filas -1; i >= 0; i--) {
-            if(tablero[i][columna] == caracter) {
-                tablero[i][columna] = ficha;
-                return i;
-            }
-        } 
-        return -1; 
-    }
-
-    private static boolean Ganador(int fila, int columna, char jugador) {
-        int[][] direcciones = {{1, 0}, {0, 1}, {1, 1}, {1, -1}};
-        for (int[] direccion : direcciones) {
-            int contador = 0;
-            for (int i = 0; i < 4; i++) {
-                int nuevaFila = fila + i * direccion[0];
-                int nuevaColumna = columna + i * direccion[1];
-                if (0 <= nuevaFila && nuevaFila < filas && 0 <= nuevaColumna && nuevaColumna < columnas &&
-                    tablero[nuevaFila][nuevaColumna] == jugador) {
-                    contador++;
-                } else {
-                    break;
-                }
-                if (contador == 4) {
-                    return true;
-                }
+    private static boolean colocarFicha(int columna) {
+        for (int i = F - 1; i >= 0; i--) {
+            if (tablero[i][columna] == '-') { 
+                tablero[i][columna] = jugadorActual; 
+                return true; 
             }
         }
         return false;
     }
 
-    private static boolean LlenarTablero() {
-        for (int i = 0; i < filas; i++) { 
-            for (int j = 0; j < columnas; j++) { 
-                if (tablero[i][j] == caracter) {
-                    return false; 
+    // Cambia el turno al otro jugador
+    
+    private static void cambiarTurno() {
+        if (jugadorActual == 'X') {
+            jugadorActual = 'O';
+        } else {
+            jugadorActual = 'X';
+        }
+    }
+
+
+    private static boolean hayGanador() {
+
+        // Verificación horizontal
+        for (int i = 0; i < F; i++) {
+            for (int j = 0; j < C - 3; j++) {
+                if (tablero[i][j] == jugadorActual && tablero[i][j + 1] == jugadorActual
+                        && tablero[i][j + 2] == jugadorActual && tablero[i][j + 3] == jugadorActual) {
+                    return true; 
                 }
             }
         }
-        return true; 
+
+        // Verificación vertical
+
+        for (int i = 0; i < F - 3; i++) {
+            for (int j = 0; j < C; j++) {
+                if (tablero[i][j] == jugadorActual && tablero[i + 1][j] == jugadorActual
+                        && tablero[i + 2][j] == jugadorActual && tablero[i + 3][j] == jugadorActual) {
+                    return true; 
+                }
+            }
+        }
+
+        // (de izquierda a derecha)
+
+        for (int i = 0; i < F - 3; i++) {
+            for (int j = 0; j < C - 3; j++) {
+                if (tablero[i][j] == jugadorActual && tablero[i + 1][j + 1] == jugadorActual
+                        && tablero[i + 2][j + 2] == jugadorActual && tablero[i + 3][j + 3] == jugadorActual) {
+                    return true; 
+                }
+            }
+        }
+
+        // (de derecha a izquierda)
+
+        for (int i = 0; i < F - 3; i++) {
+            for (int j = 3; j < C; j++) {
+                if (tablero[i][j] == jugadorActual && tablero[i + 1][j - 1] == jugadorActual
+                        && tablero[i + 2][j - 2] == jugadorActual && tablero[i + 3][j - 3] == jugadorActual) {
+                    return true; 
+                }
+            }
+        }
+
+        return false; // Si no se encuentran 4 en línea, no gana ninguno de los dos jugadores
     }
 
+    // Verifica si el tablero está lleno
+
+    private static boolean tableroLleno() {
+        for (int i = 0; i < C; i++) {
+            if (tablero[0][i] == '-') {
+                return false; 
+            }
+        }
+        return true;
+    }
+
+    // Reinicia el tablero
+    
     private static void reiniciarJuego() {
-
-        System.out.println("¿Jugar de nuevo? (s/n)");
-        if (sc.next().equalsIgnoreCase("s")) {
-            main(null);
-
-        }   else {
-            System.out.println("Gracias por jugar . ");
-        }
+        inicializarTablero(); 
+        jugadorActual = 'X'; 
+        main(null); 
     }
-    
-    
-    public static void main(String[] args) {
-                    
-        partida();
-
-        char[] jugadores = {jugador1, jugador2};
-        int jugadorActual = 0;
-
-        while (!juegoTerminado) {
-            int columna = elegirColumna(jugadores[jugadorActual]);
-            if (verificarColumna(columna)) {
-                int fila = ponerFicha(columna, jugadores[jugadorActual]);
-                if (fila == -1) {
-                    System.out.println("No se puede colocar la ficha en esta columna. Intentalo de nuevo.");
-                } else {
-                    System.out.println(" ");
-                    System.out.println(" ");
-                    estadoActual();
-                    if (Ganador(fila, columna, jugadores[jugadorActual]) || LlenarTablero()) {
-                        System.out.println(" "); 
-                        System.out.println("¡El juego ha terminado! " +
-                                (Ganador(fila, columna, jugadores[jugadorActual]) ?
-                                        "Ha ganado el jugador " + jugadores[jugadorActual] + "." :
-                                        "Es un empate"));
-                        juegoTerminado = true;
-                    } else {
-                        jugadorActual = (jugadorActual + 1) % jugadores.length;
-                    }
-                }
-            } else {
-                System.out.println(" Columna inválida. Intentalo de nuevo.");
-            }
-        }
-        reiniciarJuego();
-        }
-    }
+}
